1211c3005a4826fa9fceb7ace74edd7e29b8b345
chadkeeven <chadkeeven@gmail.com>
2018-01-09 09:01:49 -0700

    first commit

--- public/js/app.js
+++ public/js/app.js
@@ -6,40 +6,51 @@
  */
 
 
-/* hard-coded data! */
-var sampleAlbums = [];
-sampleAlbums.push({
-             artistName: 'Ladyhawke',
-             name: 'Ladyhawke',
-             releaseDate: '2008, November 18',
-             genres: [ 'new wave', 'indie rock', 'synth pop' ]
-           });
-sampleAlbums.push({
-             artistName: 'The Knife',
-             name: 'Silent Shout',
-             releaseDate: '2006, February 17',
-             genres: [ 'synth pop', 'electronica', 'experimental' ]
-           });
-sampleAlbums.push({
-             artistName: 'Juno Reactor',
-             name: 'Shango',
-             releaseDate: '2000, October 9',
-             genres: [ 'electronic', 'goa trance', 'tribal house' ]
-           });
-sampleAlbums.push({
-             artistName: 'Philip Wesley',
-             name: 'Dark Night of the Soul',
-             releaseDate: '2008, September 12',
-             genres: [ 'piano' ]
-           });
-/* end of hard-coded data */
+ // /* hard-coded data! */
+ // var sampleAlbums = [];
+ // sampleAlbums.push({
+ //   artistName: 'Ladyhawke',
+ //   name: 'Ladyhawke',
+ //   releaseDate: '2008, November 18',
+ //   genres: [ 'new wave', 'indie rock', 'synth pop' ]
+ // });
+ // sampleAlbums.push({
+ //   artistName: 'The Knife',
+ //   name: 'Silent Shout',
+ //   releaseDate: '2006, February 17',
+ //   genres: [ 'synth pop', 'electronica', 'experimental' ]
+ // });
+ // sampleAlbums.push({
+ //   artistName: 'Juno Reactor',
+ //   name: 'Shango',
+ //   releaseDate: '2000, October 9',
+ //   genres: [ 'electronic', 'goa trance', 'tribal house' ]
+ // });
+ // sampleAlbums.push({
+ //   artistName: 'Philip Wesley',
+ //   name: 'Dark Night of the Soul',
+ //   releaseDate: '2008, September 12',
+ //   genres: [ 'piano' ]
+ //});
+ /* end of hard-coded data */
 
 
 
 
-$(document).ready(function() {
+ $(document).ready(function() {
   console.log('app.js loaded!');
+  $.get("/api/albums", function(json, status){
+    json.forEach(function(album, index) {
+      renderAlbum(album);
 
+    });
+  });
+  $(".form-horizontal").submit(function(event){
+    var formData = $(this).serialize();
+      event.preventDefault();
+      $(this).reset();
+  });
+ 
 });
 
 
@@ -48,7 +59,11 @@ $(document).ready(function() {
 
 // this function takes a single album and renders it to the page
 function renderAlbum(album) {
-  console.log('rendering album:', album);
+ // console.log('rendering album:', album);
+  var name = album.name;
+  var artist = album.artistName;
+  var releaseDate = album.releaseDate;
+
 
   var albumHtml =
   "        <!-- one album -->" +
@@ -63,15 +78,15 @@ function renderAlbum(album) {
   "                    <ul class='list-group'>" +
   "                      <li class='list-group-item'>" +
   "                        <h4 class='inline-header'>Album Name:</h4>" +
-  "                        <span class='album-name'>" + "HARDCODED ALBUM NAME" + "</span>" +
+  "                        <span class='album-name'>" + name + "</span>" +
   "                      </li>" +
   "                      <li class='list-group-item'>" +
   "                        <h4 class='inline-header'>Artist Name:</h4>" +
-  "                        <span class='artist-name'>" +  "HARDCODED ARTIST NAME"+ "</span>" +
+  "                        <span class='artist-name'>" +  artist + "</span>" +
   "                      </li>" +
   "                      <li class='list-group-item'>" +
   "                        <h4 class='inline-header'>Released date:</h4>" +
-  "                        <span class='album-releaseDate'>" + "HARDCODED ALBUM RELEASE" + "</span>" +
+  "                        <span class='album-releaseDate'>" + releaseDate + "</span>" +
   "                      </li>" +
   "                    </ul>" +
   "                  </div>" +
@@ -81,5 +96,6 @@ function renderAlbum(album) {
   "          <!-- end one album -->";
 
   // render to the page with jQuery
+  $(albumHtml).appendTo('#albums');
 
 }
